a195e682c83882bfa9b5a328f66e649f07537f0a
chibo <bayoubbelmiloud@gmail.com>
2024-10-22 01:12:41 +0100

    last-update

--- android/app/src/main/java/com/flame/MainApplication.kt
+++ android/app/src/main/java/com/flame/MainApplication.kt
@@ -13,6 +13,18 @@ import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
+              // Packages that cannot be autolinked yet can be added manually here, for example:
+              // add(MyReactNativePackage())
+            }
+        override fun getJSMainModuleName(): String = "index"
+        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+      }
+
   override val reactHost: ReactHost
     get() = getDefaultReactHost(applicationContext, reactNativeHost)
 
