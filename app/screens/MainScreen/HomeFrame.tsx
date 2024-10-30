901ce7fc99e8486eb8780c201a6f63ed4af16518
Choudroi00 <bayoubbelmiloud@gmail.com>
2024-10-30 01:30:14 +0100

    update HomeFrame.tsx

--- app/screens/MainScreen/HomeFrame.tsx
+++ app/screens/MainScreen/HomeFrame.tsx
@@ -1,133 +1,137 @@
-import React, {useEffect, useCallback, useState} from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import {
-    View,
-    Image,
-    Text,
-    TouchableOpacity,
-    FlatList,
-    Pressable,
-    TouchableWithoutFeedback,
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  Pressable,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import tw from 'twrnc';
-import {useDispatch} from 'react-redux';
-import {AppDispatch} from '../../services/store/store';
-import {fetchCategories} from '../../services/store/slices/CategotiesSlice';
-import {fetchProducts} from '../../services/store/slices/ProductsSlice';
-import {switchTab} from '../../services/store/slices/MainScreenStateSlice';
-import {useTypedNavigator, useTypedSelector} from '../../utils/helpers';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../services/store/store';
+import { fetchCategories } from '../../services/store/slices/CategotiesSlice';
+import { fetchProducts } from '../../services/store/slices/ProductsSlice';
+import { switchTab } from '../../services/store/slices/MainScreenStateSlice';
+import { useTypedNavigator, useTypedSelector } from '../../utils/helpers';
 import MainTitle from '../../components/mainscreen/MainTitle';
 import HeaderAction from '../../components/mainscreen/HeaderAction';
-import {Product} from '../../models/Product';
-import Animated, {SlideInLeft} from 'react-native-reanimated';
+import { Product } from '../../models/Product';
+import Animated, { SlideInLeft } from 'react-native-reanimated';
 import ProductItem from '../../components/mainscreen/ProductItem';
 
-import {changeInCartStatus} from '../../services/store/slices/ProductsSlice';
-import {addToCart} from '../../services/store/slices/CartSlice';
+import { changeInCartStatus } from '../../services/store/slices/ProductsSlice';
+import { addToCart } from '../../services/store/slices/CartSlice';
 import XSnackbar from '../../components/common/XSnakeBar';
 import { Category } from '../../models/Category';
 
 const ITEM_TYPES = {
-    CATEGORY_HEADER: 'CATEGORY_HEADER',
-    CATEGORY_LIST: 'CATEGORY_LIST',
-    SEARCH_BAR: 'SEARCH_BAR',
-    PRODUCT_HEADER: 'PRODUCT_HEADER',
-    PRODUCT: 'PRODUCT',
+  CATEGORY_HEADER: 'CATEGORY_HEADER',
+  CATEGORY_LIST: 'CATEGORY_LIST',
+  SEARCH_BAR: 'SEARCH_BAR',
+  PRODUCT_HEADER: 'PRODUCT_HEADER',
+  PRODUCT: 'PRODUCT',
 };
 
 const HomeFrame = () => {
-    const navigator = useTypedNavigator();
-    const dispatch = useDispatch<AppDispatch>();
-    const products = useTypedSelector(state => state.products.items);
-    const categories = useTypedSelector(state => state.categories.items);
-    const userCats = useTypedSelector(state => state.user.categories)
-    const cartItems = useTypedSelector(state => state.cart.cartItems);
-    const [snv, setSnv] = useState(false);
-    
-    const catscall = useCallback(()=>{
-      const rd = categories.filter((_)=> {
-          return userCats.find((__)=> __ === _.id ) 
-      })
-      setCatRData(rd)
-    },[categories, userCats])
+  const navigator = useTypedNavigator();
+  const dispatch = useDispatch < AppDispatch > ();
+  const products = useTypedSelector(state => state.products.items);
+  const categories = useTypedSelector(state => state.categories.items);
+  const userCats = useTypedSelector(state => state.user.categories)
+  const cartItems = useTypedSelector(state => state.cart.cartItems);
+  const [snv, setSnv] = useState(false);
 
-    const [productsRenderData, setRData] = useState<Product[][]>([]);
-    const [catRenderData, setCatRData] = useState<Category[]>(catscall);
-    
-    const [data,setD] = useState([
-        {type: ITEM_TYPES.CATEGORY_HEADER},
-        {type: ITEM_TYPES.CATEGORY_LIST},
-        {type: ITEM_TYPES.SEARCH_BAR},
-        {type: ITEM_TYPES.PRODUCT_HEADER},
+  const catscall = useCallback(() => {
+    const rd = categories.filter((category) =>
+      userCats.some((userCat) => userCat === category.id)
+    );
+    setCatRData(rd);
+  }, [categories, userCats]);
+
+  const [productsRenderData, setRData] = useState < Product[][] > ([]);
+  const [catRenderData, setCatRData] = useState < Category[] > ([]);
+
+  // Call the callback function when needed
+  useEffect(() => {
+    catscall();
+  }, [catscall]);
+
+  const [data, setD] = useState([
+    { type: ITEM_TYPES.CATEGORY_HEADER },
+    { type: ITEM_TYPES.CATEGORY_LIST },
+    { type: ITEM_TYPES.SEARCH_BAR },
+    { type: ITEM_TYPES.PRODUCT_HEADER },
 
     ]);
-    
-    
 
-    useEffect(() => {
-      const rd = categories.filter((_)=> {
-            return userCats.find((__)=> __ === _.id ) 
-        })
-        setCatRData(rd)
-        setD([...data, {type: 'Default'}])
-        if (products && products?.length > 0) {
-            const filtredProducts = products.filter((_) => {
-                return userCats.find((__)=> __ === _.categories?.[0] )
-            })
-            const newData : Array<Array<Product & {isInCart: boolean}>> = new Array(Math.floor(filtredProducts.length / 2))
-                .fill(0)
-                .map((_, index) : Array<Product & {isInCart: boolean}> => {
-                    const isInCart = cartItems.some(
-                        item => item.product_id === filtredProducts[2 * index].id,
-                    );
-                    const isInCart2 = cartItems.some(
-                        item => item.product_id === filtredProducts[2 * index + 1].id,
-                    );
 
-                    return [
-                        {
-                            ...filtredProducts[2 * index],
-                            isInCart: filtredProducts[2 * index].isInCart ?? isInCart,
+
+  useEffect(() => {
+    const rd = categories.filter((_) => {
+      return userCats.find((__) => __ === _.id)
+    })
+    setCatRData(rd)
+    setD([...data, { type: 'Default' }])
+    if (products && products?.length > 0) {
+      const filtredProducts = products.filter((_) => {
+        return userCats.find((__) => __ === _.categories?.[0])
+      })
+      const newData: Array < Array < Product & { isInCart: boolean } >> = new Array(Math.floor(filtredProducts.length / 2))
+        .fill(0)
+        .map((_, index): Array < Product & { isInCart: boolean } > => {
+          const isInCart = cartItems.some(
+            item => item.product_id === filtredProducts[2 * index].id,
+          );
+          const isInCart2 = cartItems.some(
+            item => item.product_id === filtredProducts[2 * index + 1].id,
+          );
+
+          return [
+            {
+              ...filtredProducts[2 * index],
+              isInCart: filtredProducts[2 * index].isInCart ?? isInCart,
                         },
-                        {
-                            ...filtredProducts[2 * index + 1],
-                            isInCart:
-                                filtredProducts[2 * index + 1].isInCart ?? isInCart2,
+            {
+              ...filtredProducts[2 * index + 1],
+              isInCart: filtredProducts[2 * index + 1].isInCart ?? isInCart2,
                         },
                     ];
-                })
+        })
 
 
-            //setRData(newData);
-            setD((prev)=>[...prev, ...newData.map((prodCluster)=>{
-              return{
-                type: ITEM_TYPES.PRODUCT,
-                ...prodCluster
-              }
-            })])
+      //setRData(newData);
+      setD((prev) => [...prev, ...newData.map((prodCluster) => {
+        return {
+          type: ITEM_TYPES.PRODUCT,
+          ...prodCluster
         }
-    }, [products, categories, userCats]);
-
-    const toSearchTab = useCallback(() => {
-        dispatch(switchTab('search'));
-    }, [dispatch]);
-
-    const toTab = useCallback(
-        (tab: string) => {
-            dispatch(switchTab(tab));
-        },
-        [dispatch],
-    );
-
-    interface typeAndProd extends Array<Product> {
-        type: typeof ITEM_TYPES.PRODUCT;
+      })])
     }
+  }, [products, categories, userCats]);
 
-    const renderItem = useCallback(
-        ({item}: {item: typeAndProd}) => {
-            switch (item.type) {
-                case ITEM_TYPES.CATEGORY_HEADER:
-                    return (
-                        <View style={tw`flex-row justify-between p-4 px-6`}>
+  const toSearchTab = useCallback(() => {
+    dispatch(switchTab('search'));
+  }, [dispatch]);
+
+  const toTab = useCallback(
+    (tab: string) => {
+      dispatch(switchTab(tab));
+    },
+        [dispatch],
+  );
+
+  interface typeAndProd extends Array < Product > {
+    type: typeof ITEM_TYPES.PRODUCT;
+  }
+
+  const renderItem = useCallback(
+    ({ item }: { item: typeAndProd }) => {
+      switch (item.type) {
+        case ITEM_TYPES.CATEGORY_HEADER:
+          return (
+            <View style={tw`flex-row justify-between p-4 px-6`}>
                             <MainTitle text="Categories" />
                             <HeaderAction
                                 onPress={toTab}
@@ -135,10 +139,10 @@ const HomeFrame = () => {
                                 text="view all"
                             />
                         </View>
-                    );
-                case ITEM_TYPES.CATEGORY_LIST:
-                    return (
-                        <FlatList
+          );
+        case ITEM_TYPES.CATEGORY_LIST:
+          return (
+            <FlatList
                             horizontal
                             data={catRenderData}
                             renderItem={renderCategory}
@@ -148,10 +152,10 @@ const HomeFrame = () => {
                             showsHorizontalScrollIndicator={false}
                             style={tw`w-full`}
                         />
-                    );
-                case ITEM_TYPES.SEARCH_BAR:
-                    return (
-                        <View style={tw`px-4 py-2`}>
+          );
+        case ITEM_TYPES.SEARCH_BAR:
+          return (
+            <View style={tw`px-4 py-2`}>
                             <Pressable onPress={toSearchTab}>
                                 <Text
                                     style={tw`bg-gray-100 text-slate-700 font-normal text-[16px] rounded-full px-6 py-4`}>
@@ -159,30 +163,30 @@ const HomeFrame = () => {
                                 </Text>
                             </Pressable>
                         </View>
-                    );
-                case ITEM_TYPES.PRODUCT_HEADER:
-                    return (
-                        <View style={tw`flex-row justify-between p-4 px-6`}>
+          );
+        case ITEM_TYPES.PRODUCT_HEADER:
+          return (
+            <View style={tw`flex-row justify-between p-4 px-6`}>
                             <MainTitle text="Products" />
                             <HeaderAction text="view all" />
                         </View>
-                    );
-                case ITEM_TYPES.PRODUCT:
-                    return renderProduct({item});
-                default:
-                    return (
-                      <View>
+          );
+        case ITEM_TYPES.PRODUCT:
+          return renderProduct({ item });
+        default:
+          return (
+            <View>
                         
                       </View>
-                      );
-            }
-        },
+          );
+      }
+    },
         [categories, products, toSearchTab],
-    );
+  );
 
-    const renderCategory = useCallback(
-        ({item}: { item : Category}) => (
-            <TouchableWithoutFeedback
+  const renderCategory = useCallback(
+    ({ item }: { item: Category }) => (
+      <TouchableWithoutFeedback
                 onPress={() => navigator.navigate('ProductsScreen', {title: item.name ?? '', query: item.id?.toString() ?? '-1'})}
                 style={tw``}>
                 <View style={tw` w-[250px] p-2`}>
@@ -200,19 +204,19 @@ const HomeFrame = () => {
                     </Text>
                 </View>
             </TouchableWithoutFeedback>
-        ),
+    ),
         [],
-    );
+  );
 
-    const onAddToCart = (id: number) => {
-        dispatch(changeInCartStatus({id}));
-        dispatch(addToCart({productId: id, quantity: 1}));
-        setSnv(true);
-    };
+  const onAddToCart = (id: number) => {
+    dispatch(changeInCartStatus({ id }));
+    dispatch(addToCart({ productId: id, quantity: 1 }));
+    setSnv(true);
+  };
 
-    const renderProduct = useCallback(
-        ({item}: {item: Product[]}) => (
-            <View style={tw`w-full py-2 gap-x-4 px-3 flex-row`}>
+  const renderProduct = useCallback(
+    ({ item }: { item: Product[] }) => (
+      <View style={tw`w-full py-2 gap-x-4 px-3 flex-row`}>
                 <ProductItem
                     onAddToCart={onAddToCart}
                     product={item[0]}></ProductItem>
@@ -221,14 +225,14 @@ const HomeFrame = () => {
                     onAddToCart={onAddToCart}
                     product={item[1]}></ProductItem>
             </View>
-        ),
+    ),
         [],
-    );
+  );
 
-    
 
-    return (
-        <View style={tw`flex-1`}>
+
+  return (
+    <View style={tw`flex-1`}>
             {snv && (
                 <XSnackbar
                     type="success"
@@ -248,7 +252,7 @@ const HomeFrame = () => {
                 removeClippedSubviews={true}
             />
         </View>
-    );
+  );
 };
 
 export default HomeFrame;
